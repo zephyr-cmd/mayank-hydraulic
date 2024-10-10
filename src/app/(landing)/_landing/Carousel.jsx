6c955ab491fc52342e5d4ae64d853d67e1d436c0
zephyr-cmd <ts766878@gmail.com>
2024-10-10 22:19:32 +0530

    m-fix

--- src/app/(landing)/_landing/Carousel.jsx
+++ src/app/(landing)/_landing/Carousel.jsx
@@ -172,6 +172,7 @@ const Carousel = () => {
                     alt={`Service ${slide.title}`}
                     fill
                     style={{ objectFit: "cover", overflow: "hidden" }}
+                    priority={true}
                     sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw"
                   />
                 </div>
