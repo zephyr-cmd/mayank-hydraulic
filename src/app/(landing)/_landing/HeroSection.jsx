f90d1ad011232750cb32fd18cf5dccb32561e307
zephyr-cmd <ts766878@gmail.com>
2024-10-10 22:16:38 +0530

    m-fix

--- src/app/(landing)/_landing/HeroSection.jsx
+++ src/app/(landing)/_landing/HeroSection.jsx
@@ -4,7 +4,7 @@ import Carousel from "@/app/(landing)/_landing/Carousel";
 export default function HeroSection() {
   return (
     // <div className="bg-amber-400 p-2 w-full mt-10">
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto sm:mt-10">
       <Carousel />
       {/* <div className="flex flex-col sm:flex-row justify-center bg-green-200 p-5 me-5 gap-5 text-white w-full min-h-fit">
           <div className="p-4 bg-fuchsia-400 md:w-1/2 h-[600px]">Hi Simran</div>
