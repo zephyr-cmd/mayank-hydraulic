d0ef13c6eab7fbd3f9e978c6546da8e026c797b4
zephyr-cmd <ts766878@gmail.com>
2025-01-08 00:31:20 +0530

    google analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-36BEK885DT" />
     </html>
   );
 }
